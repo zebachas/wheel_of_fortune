901c1ffeef3ad0b76aa5192c573a99ee1dee5c5f
João Pereira <joaolp@pm.me>
2022-03-03 16:02:51 +0000

    first push

---
+++ src/org/terminalteam/com/Main.java
@@ -0,0 +1,15 @@
+package org.terminalteam.com;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("___________");
+
+        String row = "|O|_|_|_|_|";
+
+        System.out.println(row);
+        System.out.println(row);
+        System.out.println(row);
+        System.out.println(row);
+        System.out.println(row);
+    }
+}
